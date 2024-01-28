6beae0c9470b0d8eac8515ee1d3d4c6009b3b44b
Laxman Dahal <laxman10dahal@gmail.com>
2024-01-28 13:07:46 +0545

    file created

---
+++ main.py
@@ -0,0 +1,35 @@
+#Basic datatype
+number = 12
+point_number = 12.3
+name = "Laxman"
+my_bool = True
+
+#Derived Datatype
+list_of_name = [1,2,3,4,5,6,7]
+list_of_name = ['ram', 'shyam', 'pemba', 'rahul', 'madhu']
+
+#touple
+touple_of_num = (12,14,13,15,11)
+
+#constant
+PI = 3.1415
+
+#Dictionary
+mydict={
+    "name" : "laxman",
+    "age" : 12,
+    "address" : "Panauti"
+}
+
+list_of_set = {1,2,4,5,6,7,8,9,3},
+
+
+print(PI)
+print(point_number)
+print(name)
+print(my_bool)
+print(list_of_name)
+print(mydict)
+
+print(mydict.keys())
+print(list_of_set)
