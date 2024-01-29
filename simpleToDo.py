e877bb470f6a37f2dc3a3419b5c809d9db24714c
Laxman Dahal <laxman10dahal@gmail.com>
2024-01-29 16:39:16 +0545

    Code modified

--- simpleToDo.py
+++ simpleToDo.py
@@ -1,19 +1,22 @@
 task = []
 
+numTask = int(input("Enter how many task you want to add:  "))
+
+print("Enter tasks  ")
+
+for i in range(numTask):
+    userInput = input()
+    task.append(userInput)
+
 print("1. Add \n 2. Remove")
 operation = int(input())
-
-
-if operation == 1:
-    numTask = int(input("Enter how many task you want to add:  "))
-
-    print("Enter tasks")
-
-    for i in range(numTask):
-        userInput  = input()
-        task.append(userInput)
-if operation == 2:
-    remTask = input("Enter what you want to remove  ")
+if(operation == 1):
+    newTask = input("Enter new task  ")
+    task.append(newTask)
+elif(operation == 2):
+    remTask = input("Which you want to remove  ")  
     task.remove(remTask)
+else:
+    print("Invalid request ")
 
 print(task)
