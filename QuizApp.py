cb00a3283e256a8b29658a498558f72e217de233
Laxman Dahal <laxman10dahal@gmail.com>
2024-01-29 15:24:46 +0545

    File modified

--- QuizApp.py
+++ QuizApp.py
@@ -1,10 +1,10 @@
 quiz_questions = {
-    "What is the capital of nepal \n Kathmandu , Banepa , Panauti , Dhulikhel" : "Kathmandu",
-    "What is the height of mount everest \n 1278 , 7863 , 7632 , 8848" : "8848",
-    "where is IT bridge located \n Banepa , Panauti , Bhaktapur , Kathmandu" : "Banepa",
-    "At what age people are eligible for voting in nepal \n 16 , 23 , 18 , 34" : "18",
-    "What is the capital of india \n Kanpur , Delhi , Mumbai , Hydrabad" : "Delhi",
-    "Name one framework of python \n Django , Flask , Panda , BeautifulSoup" : "Django"
+    "What is the capital of nepal \n Kathmandu   Banepa   Panauti   Dhulikhel" : "Kathmandu",
+    "What is the height of mount everest \n 1278   7863   7632   8848" : "8848",
+    "where is IT bridge located \n Banepa   Panauti   Bhaktapur   Kathmandu" : "Banepa",
+    "At what age people are eligible for voting in nepal \n 16   23   18   34" : "18",
+    "What is the capital of india \n Kanpur   Delhi   Mumbai   Hydrabad" : "Delhi",
+    "Name one framework of python \n Django   Flask   Panda   BeautifulSoup" : "Django"
 }
 
 # questions = quiz_questions.keys()
