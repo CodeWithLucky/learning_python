b784095c2f46c3f2bb976dbd945ce2c79c000c1d
Laxman Dahal <laxman10dahal@gmail.com>
2024-01-29 13:58:30 +0545

    Quiz app created

---
+++ loop.py
@@ -0,0 +1,19 @@
+# count = 10
+# while(count>0):
+#     print(count)
+#     count = count - 1
+
+
+for i in range(10):
+    print(i)
+
+
+mydict = {"name" : "laxman", "age": 23}
+
+# mydict['phone'] = 9849275356
+
+print(mydict)
+
+# mydict.update({"age" : "22"})
+
+# print(mydict)
